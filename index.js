046a9aa2b1eeda506445a2f8cb2a98a0d9c441a4
jammie-commits <jay.mbugua.ph@gmail.com>
2024-03-22 16:48:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const writeCardsResult = writeCards(["Guadalupe", "Ollie", "Aki"]);
+
+function writeCards(names) {
+    let messages = [];
+    for (let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+console.log(writeCardsResult);
+
